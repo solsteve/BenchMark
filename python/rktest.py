603622eeff54884f168e111fb2d18ab05444abdd
Soliday <hpsp@soliday.com>
2020-03-03 16:51:27 -0500

    Comments

--- python/rktest.py
+++ python/rktest.py
@@ -5,17 +5,17 @@
 #/ **                      stephen@soliday.com                                             **
 #/ **                      http://www.soliday.com/stephen                                  **
 #/ **                                                                                      **
-#/ **  This program is free software: you can redistribute it and/or modify it under    **
-#/ **  the terms of the GNU General Public License as published by the Free Software    **
-#/ **  Foundation, either version 3 of the License, or (at your option)                 **
-#/ **  any later version.                                                               **
-#/ **                                                                                   **
-#/ **  This program is distributed in the hope that it will be useful, but WITHOUT      **
-#/ **  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS    **
-#/ **  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.   **
-#/ **                                                                                   **
-#/ **  You should have received a copy of the GNU General Public License along with     **
-#/ **  this program. If not, see <http://www.gnu.org/licenses/>.                        **
+#/ **  This program is free software: you can redistribute it and/or modify it under       **
+#/ **  the terms of the GNU General Public License as published by the Free Software       **
+#/ **  Foundation, either version 3 of the License, or (at your option)                    **
+#/ **  any later version.                                                                  **
+#/ **                                                                                      **
+#/ **  This program is distributed in the hope that it will be useful, but WITHOUT         **
+#/ **  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS       **
+#/ **  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.      **
+#/ **                                                                                      **
+#/ **  You should have received a copy of the GNU General Public License along with        **
+#/ **  this program. If not, see <http://www.gnu.org/licenses/>.                           **
 #/ **                                                                                      **
 #/ **  ----- Modification History -------------------------------------------------------  **
 #/ **                                                                                      **
@@ -171,9 +171,5 @@ def main():
 	PSG.done()
 
 #/ ==========================================================================================
-if __name__ == "__main__":
-    main()
-
-#/ ==========================================================================================
-#/ **                                 P R O P R I E T A R Y                                **
+if __name__ == "__main__":    main()
 #/ =========================================================================== END FILE =====
